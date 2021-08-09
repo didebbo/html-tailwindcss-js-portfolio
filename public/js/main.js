7906f601a894adcab3c166b8902c2cb991a2a714
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-09 17:07:40 +0200

    refactoring animazione menu

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,26 @@
 const menu = document.querySelector("#menu");
 const toggle_menu = document.querySelector("#toggle-menu");
 
-setInterval(() => {
-    if (toggle_menu === document.activeElement) {
-        toggle_menu.classList.add("focus");
-        menu.classList.add("show-menu");
-    }
-    else {
-        toggle_menu.classList.remove("focus");
-        menu.classList.remove("show-menu");
-    }
-}, 100);
+const show_menu = () => {
+    toggle_menu.classList.toggle("focus");
+    // toggle_menu.classList.add("focus");
+    menu.classList.toggle("show-menu");
+    // menu.classList.add("show-menu");
+}
+
+const hide_menu = () => {
+    toggle_menu.classList.remove("focus");
+    menu.classList.remove("show-menu");
+}
+
+// setInterval(() => {
+//     if (toggle_menu === document.activeElement) {
+//         toggle_menu.classList.add("focus");
+//         menu.classList.add("show-menu");
+//     }
+//     else {
+//         toggle_menu.classList.remove("focus");
+//         menu.classList.remove("show-menu");
+//     }
+// }, 100);
+
