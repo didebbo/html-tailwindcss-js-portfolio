7fe3be22dc509dd4dc7995144d167f8428d6b0f8
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-09 18:28:44 +0200

    realizzato multipage menu

--- public/js/main.js
+++ public/js/main.js
@@ -2,25 +2,11 @@ const menu = document.querySelector("#menu");
 const toggle_menu = document.querySelector("#toggle-menu");
 
 const show_menu = () => {
-    toggle_menu.classList.toggle("focus");
-    // toggle_menu.classList.add("focus");
-    menu.classList.toggle("show-menu");
-    // menu.classList.add("show-menu");
+    toggle_menu.classList.add("focus");
+    menu.classList.add("show-menu");
 }
 
 const hide_menu = () => {
     toggle_menu.classList.remove("focus");
     menu.classList.remove("show-menu");
 }
-
-// setInterval(() => {
-//     if (toggle_menu === document.activeElement) {
-//         toggle_menu.classList.add("focus");
-//         menu.classList.add("show-menu");
-//     }
-//     else {
-//         toggle_menu.classList.remove("focus");
-//         menu.classList.remove("show-menu");
-//     }
-// }, 100);
-
