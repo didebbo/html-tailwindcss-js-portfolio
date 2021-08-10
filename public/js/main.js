9e1eb9665a32abe5b5aeea7a9644ddcd9995b8c1
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-10 19:57:28 +0200

    refactoring dark-mode social icons

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,18 @@
+// DOM Elements
 const menu = document.querySelector("#menu");
 const toggle_menu = document.querySelector("#toggle-menu");
 const theme_mode = document.querySelector("#theme-mode");
+const icon_linkedin = document.getElementsByClassName("linkedin-icon");
+const icon_twitter = document.getElementsByClassName("twitter-icon");
+const icon_instagram = document.getElementsByClassName("instagram-icon");
+const icon_facebook = document.getElementsByClassName("facebook-icon");
 
+// Main
+window.onload = () => {
+    theme_color();
+}
+
+// Functions
 const show_menu = () => {
     toggle_menu.classList.add("focus");
     menu.classList.add("show-menu");
@@ -18,40 +29,34 @@ const theme_color = () => {
     theme_social_icons();
 }
 
-const theme_social_icons = () => {
-    const icon_linkedin = document.getElementsByClassName("linkedin-icon");
-    const icon_twitter = document.getElementsByClassName("twitter-icon");
-    const icon_instagram = document.getElementsByClassName("instagram-icon");
-    const icon_facebook = document.getElementsByClassName("facebook-icon");
+// const theme_social_icons = () => {
 
-    if (document.documentElement.classList.contains("dark")) {
-        icon_linkedin[1].classList.remove("hidden");
-        icon_twitter[1].classList.remove("hidden");
-        icon_instagram[1].classList.remove("hidden");
-        icon_facebook[1].classList.remove("hidden");
+//     if (document.documentElement.classList.contains("dark")) {
+//         icon_linkedin[1].classList.remove("hidden");
+//         icon_twitter[1].classList.remove("hidden");
+//         icon_instagram[1].classList.remove("hidden");
+//         icon_facebook[1].classList.remove("hidden");
 
-        icon_linkedin[0].classList.add("hidden");
-        icon_twitter[0].classList.add("hidden");
-        icon_instagram[0].classList.add("hidden");
-        icon_facebook[0].classList.add("hidden");
-    }
-    else {
-        icon_linkedin[0].classList.remove("hidden");
-        icon_twitter[0].classList.remove("hidden");
-        icon_instagram[0].classList.remove("hidden");
-        icon_facebook[0].classList.remove("hidden");
+//         icon_linkedin[0].classList.add("hidden");
+//         icon_twitter[0].classList.add("hidden");
+//         icon_instagram[0].classList.add("hidden");
+//         icon_facebook[0].classList.add("hidden");
+//     }
+//     else {
+//         icon_linkedin[0].classList.remove("hidden");
+//         icon_twitter[0].classList.remove("hidden");
+//         icon_instagram[0].classList.remove("hidden");
+//         icon_facebook[0].classList.remove("hidden");
 
-        icon_linkedin[1].classList.add("hidden");
-        icon_twitter[1].classList.add("hidden");
-        icon_instagram[1].classList.add("hidden");
-        icon_facebook[1].classList.add("hidden");
-    }
-}
+//         icon_linkedin[1].classList.add("hidden");
+//         icon_twitter[1].classList.add("hidden");
+//         icon_instagram[1].classList.add("hidden");
+//         icon_facebook[1].classList.add("hidden");
+//     }
+// }
 
 theme_mode.onclick = () => {
     if (localStorage.theme === "dark") localStorage.theme = "light";
     else localStorage.theme = "dark";
     theme_color();
 }
-
-theme_color();
