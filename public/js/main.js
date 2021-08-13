205f970936963a7e46ab98843811ee5e87777af8
didebbo <gianluca.napoletano.93@gmail.com>
2021-08-13 16:24:32 +0200

    terminata sezione progetti

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 // DOM Elements
+const favicon = document.querySelector("#favicon");
 const menu = document.querySelector("#menu");
 const toggle_menu = document.querySelector("#toggle-menu");
 const theme_mode_icons = document.getElementsByClassName("theme-mode-icon");
@@ -32,11 +33,13 @@ const hide_menu = () => {
 
 const theme_color = () => {
     if (localStorage.theme === "dark") {
+        favicon.href = "images/dark-icon.png";
         document.documentElement.classList.add("dark");
         theme_mode_icons[0].style.display = "none";
         theme_mode_icons[1].style.display = "inline";
     }
     else {
+        favicon.href = "images/icon.png";
         document.documentElement.classList.remove("dark");
         theme_mode_icons[0].style.display = "inline";
         theme_mode_icons[1].style.display = "none";
